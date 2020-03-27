be900ccd2b30dcb9a281e39bdd31c410932c5e65
hiroyuki.uehara <n_c_par7@yahoo.co.jp>
2020-03-27 22:34:26 +0000

    user registration

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,22 +6,12 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
     public function boot()
     {
         \Schema::defaultStringLength(191);
     }
 
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
+        public function register()
     {
         //
     }
